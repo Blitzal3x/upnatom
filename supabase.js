b31f221088dbb63d44973795a9e5fc69de09b3e4
Blitzal3x <wenigchantaljasmin@gmail.com>
2026-04-16 21:54:00 +0200

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+
+export const supabase = createClient(
+  "https://jpiedzursvtmorwljsqy.supabase.co",
+  "sb_publishable_tFNzrZxLJwSO2fJ6vOGUEQ_LTNfYoe9"
+);
